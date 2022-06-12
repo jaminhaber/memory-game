59223cb3000afddc043bed65af2c1dd579ac68c1
Benjamin <benhasanemail@gmail.com>
2022-06-12 23:07:36 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import "./App.css";
 import GameManager from "./components/GameManager";
 
 function App() {
-  return (
-    <div className="App">
-      <GameManager />
-    </div>
-  );
+  return <GameManager />;
 }
 
 export default App;
--- src/components/GameManager.js
+++ src/components/GameManager.js
@@ -1,17 +1,14 @@
 import React, { useReducer } from "react";
 import Card from "./card";
-import Counter from "./TurnCounter";
 import { spawnDeck } from "./GameLogic";
 import { reducer } from "./Reducer";
 
 const gameBoardCss = {
-  display: "flex",
-  flexDirection: "row",
-  alignItems: "stretch",
-  justifyContent: "space-around",
-  flexWrap: "wrap",
-  width: "80%",
-  margin: "0 auto"
+  display: "grid",
+  gridTemplateColumns: "1fr 1fr 1fr 1fr",
+  gridTemplateRows: "1fr 1fr 1fr",
+  width: "100%",
+  margin: "0 auto",
 };
 
 const GameManager = ({ numCards }) => {
@@ -21,27 +18,42 @@ const GameManager = ({ numCards }) => {
     deck: spawnDeck(numCards || 12),
     numClicks: 0,
     numSolved: 0,
-    refresh: false
+    refresh: false,
   });
 
   if (refresh) {
     setTimeout(() => dispatch({ type: "new-turn" }), 500);
   }
+
+  const didWin = numSolved >= winCondition;
   return (
     <div className="game">
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        {numSolved >= winCondition && (
-          <div>
-            <h1>Win!</h1>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {didWin ? (
+          <>
+            <h3 class="title">Win</h3>
             <button onClick={() => window.location.reload()}>new deck</button>
-          </div>
+          </>
+        ) : (
+          <>
+            <h3 class="title">Memory Game</h3>
+            <div className="score">solved: {numSolved}</div>
+          </>
         )}
-        <div>solved: {numSolved}</div>
       </div>
-      <div style={gameBoardCss}>
+      <div style={gameBoardCss} className="flex-1">
         {deck.map((c, i) => (
           <Card
-            onClick={() => dispatch({ type: "flip-card", payload: { index: i } })}
+            onClick={() =>
+              dispatch({ type: "flip-card", payload: { index: i } })
+            }
             key={i}
             {...c}
           />
